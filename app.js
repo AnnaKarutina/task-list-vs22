a205df1e1643d2b352cdf5bdef6436e24e0246a9
Anna Karutina <anna.karutina@khk.ee>
2022-10-20 12:51:08 +0300

    Add task functionality is created

---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#add-task')
+
+form.addEventListener('submit', addTask)
+
+function addTask(event) {
+    // get form input value
+    const taskInput = document.querySelector('#task')
+    // create li with value and X link
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(taskInput.value))
+    li.className = 'collection-item'
+
+    const x = document.createElement('a')
+    x.appendChild(document.createTextNode('X'))
+    x.setAttribute('href', '#')
+    x.className = 'secondary-content'
+
+    li.appendChild(x)
+
+    const ul = document.querySelector('ul')
+    ul.appendChild(li)
+
+    // delete input value from form input field
+    taskInput.value = ''
+    event.preventDefault()
+}
